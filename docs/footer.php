5b08f375671d83ec396b5c1493cf799603721eff
Stev888 <30306301+stev888@users.noreply.github.com>
2019-07-12 02:31:15 +0800

    Add files via upload

---
+++ docs/footer.php
@@ -0,0 +1,82 @@
+<footer>
+	<!-- footer top area start -->
+	<div class="footer-top">
+		<!-- dot animation start-->
+		<div class="footerdot dot-one"></div>
+		<div class="footerdot dot-two"></div>
+		<div class="footerdot dot-three"></div>
+		<div class="footerdot dot-four"></div>
+		<!-- dot animation end-->
+		<div class="container">
+			<div class="row">
+				<div class="col-md-3 col-sm-3 col-xs-12">
+					<div class="widget widget-logo">
+						<a href="index.html"><img src="https://oss-img.4mg.top/images/2019/mgzh/logo.png" alt="logo"></a>
+						<p>暮光之魂方块论坛是提供游戏交流、视频交流等综合化交流的论坛平台。您可在这里交流分享您的自己的体验。</p>
+						<ul>
+							<li><a href="#"><i class="fa fa-qq"></i></a></li>
+							<li><a href="#"><i class="fa fa-weixin"></i></a></li>
+							<li><a href="#"><i class="fa fa-weibo"></i></a></li>
+							<li><a href="mailto:admin@fangk.top"><i class="fa fa-envelope"></i></a></li>
+							<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+							</ul>
+					</div>
+				</div>
+				<div class="col-md-3 col-sm-3 col-xs-12">
+					<div class="widget widget-nav-menu">
+						<div class="widget_title">
+							<h2>网页文件</h2>
+						</div>
+						<ul>
+							<li><a href="#">条款和规则</a></li>
+							<li><a href="#">隐私政策</a></li>
+							<li><a href="#">免责声明</a></li>
+							<li><a href="#">版权声明</a></li>
+							<li><a href="#">帮助</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-md-3 col-sm-3 col-xs-12">
+					<div class="widget widget-nav-menu">
+						<div class="widget_title">
+							<h2>友情链接</h2>
+						</div>
+						<ul>
+							<li><a href="#">暮光互联</a></li>
+							<li><a href="#">暮光之魂官方论坛</a></li>
+							<li><a href="#">第六人格服务器租用</a></li>
+							<li><a href="#">暮光短网址</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-md-3 col-sm-3 col-xs-12">
+					<div class="widget widget-contact">
+						<div class="widget_title">
+							<h2>Contact Us</h2>
+						</div>
+						<table>
+							<tbody>
+								<tr>
+									<td>QQ群:</td>
+									<td>67989481</td>
+								</tr>
+								<tr>
+									<td>Email:</td>
+									<td>admin@fangk.top</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- footer top area start -->
+	<!-- footer bottom area start -->
+	<div class="footer-bottom">
+		<div class="container">
+			<p>Copyright &copy; 2015~2019.Twilight Soul Server rights reserved.</p>
+		</div>
+	</div>
+	<!-- footer bottom area end -->
+</footer>
---
+++ docs/header.php
@@ -0,0 +1,32 @@
+<header class="h2-header">
+	<div class="container">
+		<div class="row">
+			<div class="menu-area">
+				<div class="col-md-3 col-sm-9 col-xs-10">
+					<div class="logo"><a href="index.html" title="Prolanding - Sass & Software Landing Template"><img src="https://oss-img.4mg.top/images/2019/mgzh/logo.png" height="200px" alt="Prolanding"></a></div>
+				</div>
+				<div class="col-md-9 col-sm-9 hidden-sm hidden-xs">
+					<div class="main-menu main_nav_menu">
+						<nav class="nav-menu">
+							<ul id="mobile_menu">
+								<li class="active "><a href="http://www.fangk.top/#slider">主页</a>
+									<ul>
+										<li class="active"><a href="http://www.fangk.top/index.php">主页</a></li>
+									</ul>
+								</li>
+								<li><a href="http://www.fangk.top/#features">特征</a></li>
+								<li><a href="http://www.fangk.top/#video">动画</a></li>
+								<li><a href="http://www.fangk.top/#team">团队</a></li>
+								<li><a href="http://www.fangk.top/#joint us">加入我们</a></li>
+								<li><a href="http://www.fangk.top/exp">官网体验计划</a></li>
+							</ul>
+						</nav>
+					</div>
+				</div>
+				<div class="col-sm-3 col-xs-2 hidden-md hidden-lg">
+					<div class="responsive-menu-wrap text-right"></div>
+				</div>
+			</div>
+		</div>
+	</div>
+</header>
